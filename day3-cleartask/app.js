eb1c7d873fbdbecb8d908b6b49d719da097884c4
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-12-12 17:08:05 +1300

    Create app.js

---
+++ day3-cleartask/app.js
@@ -0,0 +1,81 @@
+//define UI
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task'); //input 
+
+//function to load all event listner
+loadEventListeners();
+
+function loadEventListeners (){
+    //add task event
+    form.addEventListener('submit',addTask);
+
+    //2. remove task event
+    taskList.addEventListener('click', removeTask);
+
+    //3. clear task event
+    clearBtn.addEventListener('click', clearTask);
+    
+};
+
+function addTask (e){
+    
+    if(taskInput.value === ''){
+        //if input is empty
+        alert('Add a task');
+    }
+
+    //Create li element
+    const li = document.createElement('li');
+    li.className='collection-item';
+    
+   //create text node and append to li
+   li.appendChild(document.createTextNode(taskInput.value));
+   //create new link element
+   const link = document.createElement('a');
+   link.className= 'delete-item secondary-content';
+   link.innerHTML='<i class="fa fa-remove"></i>';
+   li.appendChild(link);
+    
+   
+   
+    taskList.appendChild(li);
+
+    taskInput.value="";
+    
+    e.preventDefault();
+}
+
+//2.
+function removeTask (e) {
+    if(e.target.parentElement.classList.contains('delete-item')){
+        
+        if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+        }
+        
+        //console.log(e.target) = <i>
+        //e.target.parentElement = <a>
+        //e.target.parentElement.parentElement = <li>
+
+        console.log(e.target);
+    }
+}
+
+
+//3. innerHTML VS removeChild()
+//3. ClearTask (3-1)
+/*function clearTask(){
+    taskList.innerHTML = '';
+}*/
+
+//3-2. Faster way
+function clearTask(){
+    while(taskList.firstChild){
+        //while firstchile is exist = meaning if there is something in task list,
+        //we delete firstChild of tasklist
+        taskList.removeChild(taskList.firstChild);
+    }
+}
